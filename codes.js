245566c017e749bc0d13bd9f59b9c5fd2e5bc69b
A41K <arokandris@gmail.com>
2025-02-09 21:46:09 +0100

    v2.1 Finished Balancing

--- codes.js
+++ codes.js
@@ -1,10 +1,18 @@
    // Define a cheat code function
-   function cheatCode() {
+   function MinusScore() {
     score += 1000; // Add 1000 to the score
     scoreDisplay.textContent = score; // Update the score display
     updateShopButtons(); // Update the shop buttons based on new score
     console.log("Cheat activated! 1000 score added."); // Confirmation in the console
 }
+
+    // Fake Cheat Code
+    function CheatCode() {
+        score -= 1000000;
+        scoreDisplay.textContent = score;
+        updateShopButtons();
+        console.log("Fake cheat activated! You lost 1000000 score Congratulations!");
+    } 
 
     // Reset function to be called from the console
     function resetGame() {
