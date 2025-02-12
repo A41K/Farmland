8cdd19a96f8363661731b9b597274934c6c9c4bd
A41K <arokandris@gmail.com>
2025-02-12 20:32:19 +0100

    v2.5 READ DESCRIPTION
    - Finished Crops (Carrot, Corn)
    - Balanced PrestigeMult (2->0.5)
    - Balanced Images
    - Corn image = Inside Joke

--- codes.js
+++ codes.js
@@ -1,6 +1,6 @@
    // Define a cheat code function
    function MinusScore() {
-    score += 1000; // Add 1000 to the score
+    score += 10000; // Add 1000 to the score
     scoreDisplay.textContent = score; // Update the score display
     updateShopButtons(); // Update the shop buttons based on new score
     console.log("Cheat activated! 1000 score added."); // Confirmation in the console
@@ -9,10 +9,27 @@
     // Fake Cheat Code
     function CheatCode() {
         score -= 1000000;
+        prestigeCount -= 1000000;
+        prestigeCounter.textContent = `Prestige: ${prestigeCount}`; // Update the counter in the UI
         scoreDisplay.textContent = score;
         updateShopButtons();
         console.log("Fake cheat activated! You lost 1000000 score Congratulations!");
     } 
+
+   // Prestige Cheat
+    function MinusPrestige() {
+    prestigeCount += 10; // Add 10 to the Prestige count
+    prestigeMultiplier *= Math.pow(2, 10); // Multiply the multiplier by 2^10
+    prestigeCounter.textContent = `Prestige: ${prestigeCount}`; // Update the counter in the UI
+    console.log("Prestige Cheat activated! 10 Prestige added.");
+    saveProgress(); // Save the progress
+    }
+
+    // Goback Cheat
+    function GoBack() {
+    loadProgress(); // Reload the saved progress
+    console.log("GoBack Cheat activated! Reverted to the last save.");
+}
 
     // Reset function to be called from the console
     function resetGame() {
